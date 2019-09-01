e5d178eb31a88fcf28c9e0fd035fecdde8c2e1a9
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-09-01 17:43:00 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,28 +2,33 @@ class PigLatinizer
 
 
   def piglatinize(word)
-    return word if %w[and an in].include?(word) #one syllable exceptions
-    letters = word.split("")
-    letters.keep_if {|letter| letter != "."}
-    if letters.size > 0
-      until vowel?(letters[0])
-        letters << letters.shift
+      alphabet = ("A".."z").to_a
+      vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+      consonants = alphabet - vowels
+      unusable = ["i", "a", "an", "in", "on", "and"]
+      word.gsub!(/[^a-z0-9\s]/i, '')
+      word = word.split("")
+  
+  
+       if unusable.include?(word.join) || word.size <= 1
+        word.join
+      elsif vowels.include?(word[0]) && word.size > 1
+        word.join + "ay"
+      elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+        word = word.rotate.rotate.rotate << "ay"
+        word.join
+      elsif consonants.include?(word[0] && word[1])
+        word.rotate.rotate.join + "ay"
+      else
+        word.rotate.join + "ay"
       end
-      letters  << "ay"
     end
-    letters.join
-  end
 
-   def to_pig_latin(text)
-    words = text.split(" ")
-    words.map! {|word| piglatinize(word)}
-    words.join(" ")
-   end
-
-  def vowel?(letter)
-    letter.downcase
-    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
-  end
-
+    def to_pig_latin(words)
+      result = words.split(" ").map do |word|
+      piglatinize(word)
+      end
+      result.join(" ")
+    end
 
 end
